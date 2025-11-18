59d88ac27615be9919c4511291e794360b61c175
rakshit-sha <rakshit.sharma@avisoft.io>
2025-11-18 14:37:44 +0530

    string formatting

--- operators&IO/string.py
+++ operators&IO/string.py
@@ -3,9 +3,16 @@ favTech = input('Enter your favourite technology: ')
 
 print("Hello,", name,"your favourite technology is", favTech,".")
 
-# using formatted string
-print('f-string')
+# formatted string
+print('using f-string')
 print(f"Hello {name} your favourite technology is {favTech}.")
-print("Hello {} your favourite technology is {}." .format(name,favTech))
-print("Hello %s your favourite technology is %s." %(name,favTech))
 
+print('using format method')
+print("Hello {} your favourite technology is {}." .format(name,favTech))
+# print("Hello %s your favourite technology is %s." %(name,favTech))
+
+print(name.upper())
+
+# modifiers
+print(f'The price for its course is {45000:.2f}')
+print(f'The price for its course is {45000:,}')
