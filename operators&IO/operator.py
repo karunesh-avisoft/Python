5f9725bd16464aa0c59b7ac987c123240c90956b
karunesh-avisoft <karunesh.tiwari@avisoft.io>
2025-11-18 19:13:51 +0530

    practice & list

---
+++ operators&IO/operator.py
@@ -0,0 +1,13 @@
+# fruits = list(("apple","banana","cherry","date","elderberry"))
+fruits = ["apple","banana","cherry","date","elderberry"]
+print(fruits)
+print('3rd item:',fruits[2])
+print('last item:',fruits[-1])
+print('second last item:',fruits[-2])
+
+# modify
+colors = ["red","blue","green","yellow"]
+colors[1]= "purple"
+colors[-1]="orange"
+
+print('colors:',colors)
