6cce6ce93ff51d7bf6d3825d0d563a9b8dd1d0e6
karunesh-avisoft <karunesh.tiwari@avisoft.io>
2025-12-02 18:27:00 +0530

    modularise capstone

--- Assignments/Capstone/inventory_management_system.py
+++ Assignments/Capstone/inventory_management_system.py
@@ -1,8 +1,15 @@
+from datetime import date
 from pathlib import Path
+import threading
 from dotenv import load_dotenv
+from logger import logging
+import random
 import os
 import json
-from logger import logging
+from utilities import *
+# for scheduling weekly backups
+import schedule
+import time
 
 # load variables from .env
 load_dotenv()
@@ -16,62 +23,98 @@ backup = directory / os.getenv('BACKUP_FILE')
 # Ensure the directory exists
 directory.mkdir(parents=True, exist_ok=True)
 
-logging.basicConfig(
-    filename = transactions,
-    level = logging.INFO,
-    format = "%(asctime)s - %(levelname)s - %(message)s"
-)
 
-# FILE HANDLING
+# DATA PERSISTENCE FUNCTIONS
 def load_data():
     try:
         with open(inventory, 'r') as f:
             json_data = json.load(f)
-        logging.info("Inventory data loaded successfully.")
+        logging.debug("Inventory data loaded successfully.")
         return json_data
     except FileNotFoundError:
-        logging.error(f"The file '{inventory}' was not found.")
+        logging.warning("Inventory file not found. Returning empty inventory.")
         return {}
-    except json.JSONDecodeError as e:
-        logging.error(e)
+    except json.JSONDecodeError:
+        logging.error("Error decoding inventory JSON data. Returning empty inventory.")
+        return {}
+    except PermissionError:
+        logging.error("Permission denied while accessing inventory file. Returning empty inventory.")
+        return {}
     except Exception as e:
         logging.error(e)
+        return {}
 
 def save_data(inventory_data={}):
     try:
         with open(inventory, 'w') as f:
             json.dump(inventory_data, f, indent=4)
-        logging.info("Inventory data saved successfully.")
+        backup_data()  # create backup after every save
+        logging.debug("Inventory data saved successfully.")
+    except FileNotFoundError:
+        logging.warning("Inventory file not found. Unable to save data.")
+    except json.JSONDecodeError:
+        logging.error("Error decoding inventory JSON data. Unable to save data.")
+    except PermissionError:
+        logging.error("Permission denied while accessing inventory file. Unable to save data.")
     except Exception as e:
         logging.error(e)
 
-# BACKUP HANDLING
-# def backup_data():
-#     try:
-#         with open(inventory, 'r') as f:
-#             data = json.load(f)
-#         with open(backup, 'w') as f:
-#             json.dump(data, f, indent=4)
-#         logging.info("Backup created successfully.")
-#     except Exception as e:
-#         logging.error(e)
 
-# RESTORE HANDLING
-# def restore_data(): 
-#     try:
-#         with open(backup, 'r') as f:
-#             data = json.load(f)
-#         with open(inventory, 'w') as f:
-#             json.dump(data, f, indent=4)
-#         logging.info("Data restored from backup successfully.")
-#     except Exception as e:
-#         logging.error(e)
+# BACKUP & RESTORE HANDLING
+def backup_data():
+    try:
+        with open(inventory, 'r') as f:
+            data = json.load(f)
+        with open(backup, 'w') as f:
+            json.dump(data, f, indent=4)
+        logging.debug("Backup created successfully.")
+    except Exception as e:
+        logging.error(e)
+
+def restore_data(): 
+    try:
+        with open(backup, 'r') as f:
+            data = json.load(f)
+        with open(inventory, 'w') as f:
+            json.dump(data, f, indent=4)
+        logging.info("Data restored from backup successfully.")
+    except Exception as e:
+        logging.error(e)
+
+# WEEKLY BACKUP SCHEDULER
+def weekly_backup():
+    logging.info("Running scheduled weekly backup...")
+    backup_data()
+
+# scheduler thread
+def run_scheduler():
+    schedule.every().monday.at("02:00").do(weekly_backup)
+    # schedule.every(10).seconds.do(weekly_backup)  # for testing, runs every 10 seconds       
+
+    while not stop_scheduler.is_set():
+        schedule.run_pending()
+        time.sleep(1)
+
+# scheduler control functions
+stop_scheduler = threading.Event() # <-- stop signal
+
+def start_scheduler():
+    global scheduler_thread
+    scheduler_thread = threading.Thread(target=run_scheduler, daemon=True)
+    scheduler_thread.start()
+    print("Scheduler started.")
+
+def stop_scheduler_thread():
+    stop_scheduler.set()       # <-- stop signal
+    scheduler_thread.join()    # waits for thread to exit
+    print("Scheduler stopped.")
+
 
 # TRANSACTION LOG MANAGEMENT
 def log_transaction(t_type, product_id, qty_change, previous_qty):
     try:
-        log_entry = f"{t_type} | Product ID: {product_id} | Change: {qty_change} | Previous Qty: {previous_qty}\n"
-        logging.log(f"{log_entry.strip()}")
+        log_entry = f"{t_type} | Product ID: {product_id} | Change in Quantity(+/-): {qty_change} | Previous Qty: {previous_qty}\n"
+        logging.info(f"{log_entry.strip()}")
     except Exception as e:
         logging.error(e)
 
@@ -79,8 +122,149 @@ def view_transactions():
     try:
         with open(transactions, 'r') as f:
             logs = f.readlines()
-        for log in logs:
-            print(log.strip())
-        logging.info("Transaction log viewed successfully.")   
+            
+            print("\n"+"="*30, "TRANSACTION LOGS", "="*30)
+            for log in logs: 
+                    print(log.strip())
+            print("="*78)
+        logging.debug("Transaction log viewed successfully.")   
     except Exception as e:
         logging.error(e)
+
+# additional function to clear transaction log
+def clear_transactions():
+    try:
+        open(transactions, 'w').close()
+        logging.debug("Transaction log cleared successfully.")
+    except Exception as e:
+        logging.error(e)
+    
+
+# INVENTORY MANAGEMENT FUNCTIONS
+def add_new_product():
+    inv_data = load_data()
+    # Get product details
+    product_details = validate_product_data()
+
+    # Generate unique product ID
+    product_id=str(random.randint(1,5000))
+    # Ensure unique product ID
+    while product_id in inv_data:
+        product_id=str(random.randint(1,5000))
+
+    # Add to inventory 
+    try:
+        for values in inv_data.values():
+            # check for duplicate product names
+            if product_details['name'].lower() == values['name'].lower():
+                logging.warning("Product with this name already exists in inventory. Use update stock option instead.")
+        inv_data[product_id] = product_details
+        save_data(inv_data)
+        log_transaction('NEW_ITEM', product_id, product_details['quantity'], 0)
+    except Exception as e:
+        logging.error(e)
+    
+def update_stock(product_id, qty_change):
+    inv_data = load_data()
+    try:
+        if product_id in inv_data:
+            previous_qty = inv_data[product_id]['quantity']
+            if inv_data[product_id]['quantity'] + qty_change < 0:
+                logging.warning(f"Insufficient stock to remove the specified quantity. Available quantity: {inv_data[product_id]['quantity']}")
+                return
+            inv_data[product_id]['quantity'] += qty_change
+            if inv_data[product_id]['quantity']==0:
+                delete_product(product_id)
+            else:   
+                inv_data[product_id]['last_restock'] = date.today().isoformat()
+                save_data(inv_data)
+            log_transaction('STOCK_UPDATE', product_id, qty_change, previous_qty)
+        else:
+            logging.warning(f"Product ID {product_id} not found in inventory.")
+    except Exception as e:
+        logging.error(e)
+
+def search_product(search_term):
+    inv_data = load_data()
+    results = {}
+    try:
+        for pid, details in inv_data.items():
+            if (search_term.lower() in details['name'].lower()) or (search_term.lower() in details['category'].lower()):
+                results[pid] = details
+        if results:
+            logging.info(f"Found {len(results)} matching products.")
+            return results
+        else:
+            logging.info("No matching products found.")
+    except Exception as e:
+        logging.error(e)
+        return {}
+
+# additional function to delete a product  
+def delete_product(product_id):
+    inv_data = load_data()
+    try:
+        if product_id in inv_data:
+            change_qty = -inv_data[product_id]['quantity']
+            previous_qty = inv_data[product_id]['quantity']
+            del inv_data[product_id]
+            save_data(inv_data)
+            log_transaction('DELETE_ITEM', product_id, change_qty, previous_qty)
+        else:
+            logging.warning(f"Product ID {product_id} not found in inventory.")
+    except Exception as e:
+        logging.error(e)
+
+
+# REPORTING FUNCTIONS
+def generate_inventory_report():
+    inv_data = load_data()
+    try:
+        total_items = len(inv_data)
+        total_value = sum(details['quantity'] * details['price'] for details in inv_data.values())
+        low_stock = sum(1 for details in inv_data.values() if details['quantity'] < 10)
+        out_of_stock = sum(1 for details in inv_data.values() if details['quantity'] <= 0)
+
+        # lambda to find most stocked item
+        most_stocked_item = max(inv_data.items(), key=lambda x: x[1]['quantity']) if inv_data else (None, None)
+        # print("Most stocked:", most_stocked_item) --> ('106', {'name': 'Ball Pen Pack (10 pcs)', 'category': 'Stationery', 'quantity': 200, 'price': 75.0, 'last_restock': '2025-02-01'})
+
+        # Display detailed inventory
+        print("\n"+"="*30, "INVENTORY REPORT", "="*30)
+        print(f"Total Unique Items: {total_items}")
+        print(f"Total Inventory Value: INR{total_value:.2f}")
+        print(f"Low Stock Items (qty<10): {low_stock}")
+        print(f"Out of Stock Items: {out_of_stock}")
+        print(f"Most Stocked Category: {most_stocked_item[1]['category'] if most_stocked_item[1] else 'N/A'} (Qty: {most_stocked_item[1]['quantity'] if most_stocked_item[1] else 'N/A'})")
+        print("="*78)
+
+        logging.info("Inventory report generated successfully.")
+    except Exception as e:
+        logging.error(e)
+
+def display_inventory(format='table', category=None, stock_status=None):
+    inv_data = load_data()
+    # filtered data based on category and stock status
+    filtered_data={}
+    for pid, details in inv_data.items():
+        if category and category.lower() not in inv_data[pid]['category'].lower():
+            continue
+        if stock_status=='in_stock' and inv_data[pid]['quantity']<=0:
+            continue
+        if stock_status=='out_of_stock' and inv_data[pid]['quantity']>0:
+            continue
+        filtered_data[pid]=details
+
+    try:
+        if format=='table':
+            display_data(filtered_data, 'INVENTORY DATA')
+        else:
+            print("\n"+"="*33, "INVENTORY DATA", "="*33)
+            for pid, details in filtered_data.items():
+                print(f"ID: {pid} | Name: {details['name']} | Category: {details['category']} | Quantity: {details['quantity']} | Price: {details['price']} | Last Restock: {details['last_restock']}")
+            print("="*84)
+            
+        logging.info("Inventory viewed successfully.")
+    except Exception as e:
+        logging.error(e)
+
--- Assignments/Capstone/main.py
+++ Assignments/Capstone/main.py
@@ -1,7 +1,88 @@
 from inventory_management_system import *
+from utilities import *
 
-# loaded_data = load_data()
-# # formatted clean print
-# print(json.dumps(loaded_data, indent=4))
-# # save_data({'item1': 10, 'item2': 20})
-log_transaction('NEW_ITEM',101,50,0)
+def main():
+    # start the weekly backup scheduler
+    start_scheduler()
+
+    consent = 'y'
+    while consent == 'y':
+        print("\n" + "="*31)
+        print(" INVENTORY MANAGEMENT SYSTEM ")
+        print("="*31)
+        print("1. View Current Inventory")
+        print("2. Add New Product")
+        print("3. Update Product Stock")
+        print("4. Search Product")
+        print("5. Delete Product") 
+        print("6. Generate Inventory Report")
+        print("7. View Transaction Logs")
+        # print("8. Backup Data") auto incremental backup after every save
+        print("8. Restore Data")
+        print("9. Exit")
+        print("="*30) 
+        try:
+            # get user choice
+            choice = input("\nEnter your choice (1-9): ").strip()
+            while not choice.isdigit() or int(choice) not in range(1, 10):
+                choice = input("Invalid choice. Please enter a number between 1 and 9: ").strip()
+
+            # process user choice
+            if choice == '1':
+                display_inventory()
+
+            elif choice == '2':
+                while True:
+                    add_new_product()
+                    another = input("\nDo you want to add another product? (y/n): ").strip().lower()
+                    while another not in ['y', 'n']:    
+                        another = input("Invalid input. Please enter 'y' for yes or 'n' for no: ").strip().lower()
+                    if another == 'n':
+                        break
+
+            elif choice == '3':
+                product_id = input("Enter product ID to update stock: ").strip()
+                qty_change = int(input("Enter quantity change (positive to add, negative to remove): ").strip())
+                update_stock(product_id, qty_change)
+
+            elif choice == '4':
+                search_term = input("Enter product name or category to search: ").strip()
+                found_data = search_product(search_term)
+                if found_data:
+                    display_data(found_data, 'SEARCH RESULTS')
+
+            elif choice == '5':
+                product_id = input("Enter product ID to delete: ").strip()
+                delete_product(product_id)
+
+            elif choice == '6':
+                generate_inventory_report()
+
+            elif choice == '7':
+                view_transactions()
+
+            elif choice == '8':
+                restore_data()
+
+            elif choice == '9':
+                print("*** Exiting the system. Goodbye! ***")
+                break
+
+            # get user consent for another operation
+            consent = input("\nDo you want to perform another operation? (y/n): ").strip().lower()
+            while consent not in ['y', 'n']:
+                consent = input("Invalid input. Please enter 'y' for yes or 'n' for no: ").strip().lower()
+
+        # except keyboard and EOF errors to exit gracefully
+        except (KeyboardInterrupt, EOFError):
+            print("\n*** Exiting the system. Goodbye! ***")
+            break
+    
+    # stop the scheduler thread before exiting
+    stop_scheduler_thread()
+
+
+
+# run the main function
+if __name__ == '__main__':
+    main()
